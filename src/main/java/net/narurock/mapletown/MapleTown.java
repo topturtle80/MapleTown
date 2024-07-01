4f23e5a04b33e82e64a6887fd65d390b602fdda4
toptu <topturtle80@gmail.com>
2024-06-30 22:46:26 -0700

    aps

--- src/main/java/net/narurock/mapletown/MapleTown.java
+++ src/main/java/net/narurock/mapletown/MapleTown.java
@@ -1,6 +1,7 @@
 package net.narurock.mapletown;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,6 +12,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.narurock.mapletown.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -22,7 +24,11 @@ public class MapleTown {
     public MapleTown() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
+
         modEventBus.addListener(this::commonSetup);
+
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
     }
@@ -33,7 +39,9 @@ public class MapleTown {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.mapplite_ingot);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/narurock/mapletown/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.narurock.mapletown.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.narurock.mapletown.MapleTown;
+
+public class ModItems {
+    public static final DeferredRegister<Item> Items =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MapleTown.MOD_ID);
+
+    public static final RegistryObject<Item> mapplite_ingot = Items.register("mapplite_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventbus) {
+        Items.register(eventbus);
+    }
+}
